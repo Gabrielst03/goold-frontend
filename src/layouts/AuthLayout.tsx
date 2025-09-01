7253001fdf0bfd3bfc9b782f334a3e8d0b2dc438
Gabriel Santana <santanag311@gmail.com>
2025-09-01 17:21:20 -0300

    feat: add dinamic title to button on the AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React, { ReactNode } from 'react'
 
 
@@ -8,11 +10,19 @@ interface AuthLayout {
 }
 
 export default function AuthLayout({ children }: AuthLayout) {
+
+    const pathname = usePathname()
+
+    const TitleButton = pathname === '/signin' ? 'Cadastre-se' : 'Login'
+    const HrefButton = pathname === '/signin' ? '/signup' : '/signin'
+
     return (
         <>
             <header className='flex items-center justify-between w-full px-25 py-5 border-b'>
                 <Image src={'/logo.png'} alt='Logo' width={43} height={43} />
-                <Button size={'lg'}>Cadastre-se</Button>
+                <Link href={HrefButton}>
+                    <Button size={'lg'}>{TitleButton}</Button>
+                </Link>
             </header>
 
             <div className='flex flex-col items-center justify-center mt-36'>
